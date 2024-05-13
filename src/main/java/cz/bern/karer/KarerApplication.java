4783bfa305f18fc74fb10bc33e863c36ae7038ab
Nikolas5456587 <Nikbernat10@gmail.com>
2024-05-13 17:11:04 +0200

    1st commit

---
+++ src/main/java/cz/bern/karer/KarerApplication.java
@@ -0,0 +1,13 @@
+package cz.bern.karer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/bern/karer/KarerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.bern.karer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
